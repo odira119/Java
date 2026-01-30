4d7001d87fe3241c19939ec8da9ecae7148e075f
Frankline Odira <franklineodira2005@gmail.com>
2026-01-30 13:17:35 +0300

    commit -m all project files

--- src/AppletHelloWord.java
+++ src/AppletHelloWord.java
@@ -1,8 +1,19 @@
-import java.applet.Applet;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 import java.awt.Graphics;
 
-public class AppletHelloWord extends Applet {
-    public void paint(Graphics g) {
+public class AppletHelloWord extends JPanel {
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
         g.drawString("Hello World", 20, 20);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        AppletHelloWord panel = new AppletHelloWord();
+        frame.add(panel);
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
     }
 }
